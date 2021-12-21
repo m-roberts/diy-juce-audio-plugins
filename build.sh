b271082fa77f18d03bd31c424d80d853cfdf9137
m-roberts <m-roberts@users.noreply.github.com>
2021-12-21 00:54:56 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,25 +3,37 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 mkdir -p ${DIR}/artefacts
+rm -rf ${DIR}/artefacts/*
 
-for dir in ./src/plugin/*; do
+for dir in ${DIR}/src/plugin/*; do
   (
-    cd ${dir}
-    ln -s ${DIR}/lib/JUCE .
+    # Clean JUCE submodule
+    cd ${DIR}/lib/JUCE
+    git clean -fdx
 
+    # Symlink JUCE to project
+    [[ -e JUCE ]] || ln -s ${DIR}/lib/JUCE ${dir}
+
+    # Build
+    cd "${dir}"
     cmake .
     make -j$(sysctl -n hw.logicalcpu)
 
+    # Store
     suffix_to_remove="*_artefacts"
     artefacts_dir="$(find . -name "${suffix_to_remove}")"
 
-    mv "${artefacts_dir}" "${DIR}/artefacts/$(echo ${artefacts_dir} | sed 's/_artefacts//')"
-    rm -rf ./CMakeCache.txt ./CMakeFiles JUCE Makefile cmake_install.cmake
-
-    cd ${DIR}/lib/JUCE
-    git clean -fdx
+    new_artefacts_dir="${DIR}/artefacts/$(echo ${artefacts_dir} | sed 's/_artefacts//')"
+    mv "${artefacts_dir}" "${new_artefacts_dir}"
+    rm -rf \
+      ${dir}/{CMakeCache.txt,CMakeFiles,JUCE,Makefile,cmake_install.cmake} \
+      ${new_artefacts_dir}/{JuceLibraryCode,Debug,lib*_SharedCode.a}
   )
 done
 
 # Clean up undesirable artefact files
 rm -rf ${DIR}/artefacts/*/{JuceLibraryCode,Debug,lib*_SharedCode.a}
+
+# Final JUCE cleanup
+cd ${DIR}/lib/JUCE
+git clean -fdx
