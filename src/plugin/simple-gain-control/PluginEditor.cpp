62146bbec29c3da7df90b1c11292af4419f4d27a
m-roberts <m-roberts@users.noreply.github.com>
2021-12-21 01:07:54 +0000

    Add 'gain' label

--- src/plugin/simple-gain-control/PluginEditor.cpp
+++ src/plugin/simple-gain-control/PluginEditor.cpp
@@ -51,9 +51,9 @@ void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    // g.setColour (juce::Colours::white);
-    // g.setFont (15.0f);
-    // g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Gain", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void NewProjectAudioProcessorEditor::resized()
